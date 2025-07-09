ec6ef405cfadf7405a66a9919f1c57b31f4c9dfc
chipotleman <mathewbaldwin98@icloud.com>
2025-07-08 23:02:28 -0400

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,12 @@
 // pages/dashboard.js
 
-import { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
+
 
 export default function Dashboard() {
   const [games, setGames] = useState([]);
