be170e44b8a9c07f7c0ad18bcefe42a5796bbc1e
chipotleman <mathewbaldwin98@icloud.com>
2025-07-09 20:58:56 -0400

    Create test.js

---
+++ pages/test.js
@@ -0,0 +1,7 @@
+export default function Test() {
+    return (
+        <div className="min-h-screen bg-black text-green-400 flex justify-center items-center text-4xl">
+            ✅ Tailwind is working 🚀
+        </div>
+    );
+}
