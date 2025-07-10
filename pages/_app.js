d6e7cd125b577d1164073774dabc92965b2d17c0
chipotleman <mathewbaldwin98@icloud.com>
2025-07-09 21:16:50 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+// pages/_app.js
+
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>RollrFunded Sportsbook</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Funded sports betting platform by RollrFunded" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
