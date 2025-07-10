b0e549b01193dd70108ab517626bbb731d58d22a
chipotleman <mathewbaldwin98@icloud.com>
2025-07-10 13:16:36 -0400

    Update route.ts

--- app/api/settleBets/route.ts
+++ app/api/settleBets/route.ts
@@ -1,6 +1,6 @@
 // app/api/settleBets/route.ts
 
-import { supabase } from '@/lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 
 export async function POST(req: Request) {
   try {
