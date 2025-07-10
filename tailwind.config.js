6c398e021966aedaebd4a25434909873e58dee57
chipotleman <mathewbaldwin98@icloud.com>
2025-07-09 20:51:14 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'neon-green': '#39FF14',
+        'neon-blue': '#00FFFF',
+      },
+    },
   },
   plugins: [],
 }
-
