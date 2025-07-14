a707a84d814faea3d18b0208b3539355e3afd040
chipotleman <mathewbaldwin98@icloud.com>
2025-07-14 16:43:42 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css';
-import TopNavbar from '@/components/TopNavbar';
+import '../styles/globals.css';
+import TopNavbar from '../components/TopNavbar';
 import { useState } from 'react';
 
 export default function App({ Component, pageProps }) {
